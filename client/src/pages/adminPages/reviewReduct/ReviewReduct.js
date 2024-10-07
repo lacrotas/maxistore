8c90bb74fa408dbb9a5a6956e4d5416dcaabb97f
lacrotas <123123maks1@mail.ru>
2024-10-08 00:53:40 +0300

    reviewReduct

--- client/src/pages/adminPages/reviewReduct/ReviewReduct.js
+++ client/src/pages/adminPages/reviewReduct/ReviewReduct.js
@@ -5,15 +5,27 @@ import { useState, useEffect } from "react";
 import CustomButton from "../../../customUI/customButton/CustomButton";
 import ModalWindow from "./modalWindow/ModalWindow";
 import "./ReviewReduct.scss";
+import CustomLabel from "../../../customUI/customLabel/CustomLabel";
 
 function ReviewReduct() {
     const [reviews, setReviews] = useState([]);
     const [isModalActive, setIsModalActive] = useState(false);
     const [activeReviews, setActiveReview] = useState("");
 
+    const [isShowedReviews, setIsShowedReviews] = useState(true);
+    const [isShowedReviewsNumber, setIsShowedReviewsNumber] = useState(false);
+    const [unShowedReviewsNumber, setUnShowedReviewsNumber] = useState(false);
+
     useEffect(() => {
         fetchAllReview().then(data => {
             setReviews(data);
+            data.map((item) => {
+                if (item.isShowed) {
+                    setIsShowedReviewsNumber(true);
+                } else if (item.isShowed == false) {
+                    setUnShowedReviewsNumber(true);
+                }
+            })
         })
     }, [])
 
@@ -26,16 +38,35 @@ function ReviewReduct() {
         <>
             {isModalActive && <ModalWindow setIsModalActive={setIsModalActive} reviewItem={activeReviews} />}
             <Headers isAdminHeader={true} />
+            <CustomLabel text={"Редактирование отзывов"} />
             {reviews.length > 0 ?
                 (
                     <div className="review_reduct">
-                        {reviews.map((item, index) => (
-                            <div className="review_reduct_item" key={index}>
-                                <p className="tiny_p">{item.label}</p>
-                                <p className="tiny_p">{item.isShowed ? "показан" : "скрыт"}</p>
-                                <CustomButton dealOnClick={reductReview} value={item} text={"Редактировать"} />
-                            </div>
-                        ))}
+                        <div className="review_reduct_header">
+                            <p className={`reduct_header_paragraph small_p ${isShowedReviews ? "active" : ""}`} onClick={() => setIsShowedReviews(true)}>Одобренные отзывы</p>
+                            <p className={`reduct_header_paragraph small_p ${!isShowedReviews ? "active" : ""}`} onClick={() => setIsShowedReviews(false)}>Ожидающие одобрения отзывы</p>
+                        </div>
+                        <div className={`reduct_header_container ${isShowedReviews ? "active" : ""}`}>
+
+                            {isShowedReviewsNumber ? reviews.map((item, index) => (
+                                item.isShowed ?
+                                    <div className="review_reduct_item" key={index}>
+                                        <p className="tiny_p">{item.label}</p>
+                                        <CustomButton dealOnClick={reductReview} value={item} text={"Редактировать"} />
+                                    </div>
+                                    : <></>
+                            )) : <p className="review_reduct_paragraph tiny_p">Пока у вас нет отзывов</p>}
+                        </div>
+                        <div className={`reduct_header_container ${!isShowedReviews ? "active" : ""}`}>
+                            {unShowedReviewsNumber ? reviews.map((item, index) => (
+                                !item.isShowed ?
+                                    <div className="review_reduct_item" key={index}>
+                                        <p className="tiny_p">{item.label}</p>
+                                        <CustomButton dealOnClick={reductReview} value={item} text={"Редактировать"} />
+                                    </div>
+                                    : <></>
+                            )) : <p className="review_reduct_paragraph tiny_p">Пока нет отзывов требующих одобрение</p>}
+                        </div>
                     </div>
                 )
                 :
